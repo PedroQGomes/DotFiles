b2941264d0d8addddf0661aca31b9b956adcd0b4
PedroQGomes <pedromqueiros1999@gmail.com>
2020-09-13 18:31:07 +0100

    routes to components details added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
+  
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,20 +1,18 @@
 export const products = [
     {
+      id: 'gmjwrngu123',
       name: 'Phone XL',
       price: 799,
       description: 'A large phone with one of the best screens'
     },
     {
+      id: 'asfnajsf657',
       name: 'Phone Mini',
       price: 699,
       description: 'A great phone with one of the best cameras'
     },
     {
-      name: 'Phone Standard',
-      price: 299,
-      description: 'who cares'
-    },
-    {
+      id: '1278fn1mgafnh',
       name: 'Iphone 10',
       price: 100000,
       description: 'overrated af'
