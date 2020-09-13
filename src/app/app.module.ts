a648f18133b75443b8fdda2eb2c93f003be12276
PedroQGomes <pedromqueiros1999@gmail.com>
2020-09-13 23:13:15 +0100

    added shopping cart and mutch more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component'
+
 
 @NgModule({
   
@@ -17,15 +21,20 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart',component: CartComponent},
+      { path: 'shipping',component: ShippingComponent},
     ])
   ],
   providers: [],
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products} from '../products';
+import { products } from '../products';
+import { CartService } from "../cart.service";
 
 
 
@@ -15,12 +16,21 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private cart: CartService,
     ) { }
+
+
+
+    addToCart(product){
+      this.cart.setItem(product);
+      window.alert(product.name + " has been added to the cart !");
+    }
+
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
 
-      this.product = products[+params.get('product.id')];
+      this.product = products[+params.get('productId')];
 
 
     })
