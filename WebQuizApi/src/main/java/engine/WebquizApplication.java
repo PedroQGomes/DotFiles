141fce3807f1c7fe33ffbeec5cfb18a21707b225
PedroQGomes <pedromqgomes@gmail.com>
2021-02-13 19:09:40 +0000

    restruturaçao da web api

--- WebQuizApi/src/main/java/engine/WebquizApplication.java
+++ WebQuizApi/src/main/java/engine/WebquizApplication.java
@@ -3,12 +3,11 @@ package engine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-public class WebQuizEngine {
+public class WebquizApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebQuizEngine.class, args);
+        SpringApplication.run(WebquizApplication.class, args);
     }
 
 }
