cb0f86ed898e3ac5dbbbc7747cf0886be5e4c1b6
PedroQGomes <pedromqgomes@gmail.com>
2021-11-09 23:59:25 +0000

    server side rendering

---
+++ RedditApp/client/src/utils/createUrqlClient.ts
@@ -0,0 +1,78 @@
+import { cacheExchange, Resolver, Cache } from "@urql/exchange-graphcache";
+import {
+  dedupExchange,
+  Exchange,
+  fetchExchange,
+  stringifyVariables,
+} from "urql";
+import { pipe, tap } from "wonka";
+import {
+  LoginMutation,
+  LogoutMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
+//import { betterUpdateQuery } from "./betterUpdateQuery";
+import Router from "next/router";
+import gql from "graphql-tag";
+//@ts-checkimport { isServer } from "./isServer";
+
+
+
+
+
+export const createUrqlClient = (_ssrExchange:any) => {
+  url: 'http://localhost:4000/graphql',
+  fetchOptions:{
+    credentials:"include"
+  },
+  exchanges: [dedupExchange, cacheExchange({
+    updates:{
+      Mutation:{
+          logout: (_result, args, cache, info) =>{
+              betterUpdateQuery<LogoutMutation,MeQuery>(
+                cache,
+                {query:MeDocument},
+                _result,
+                () => ({me:null})
+
+              )
+          },
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation,MeQuery>(
+              cache,
+              {query:MeDocument},
+              _result,
+              (result,query) => {
+                  if(result.login.errors){
+                    return query
+                  }else{
+                    return {
+                      me: result.login.user
+                    };
+                  }
+              }
+            )
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation,MeQuery>(
+              cache,
+              {query:MeDocument},
+              _result,
+              (result,query) => {
+                  if(result.register.errors){
+                    return query
+                  }else{
+                    return {
+                      me: result.register.user
+                    };
+                  }
+              }
+            )
+          },
+      }
+    }
+
+  }), fetchExchange],
+}
